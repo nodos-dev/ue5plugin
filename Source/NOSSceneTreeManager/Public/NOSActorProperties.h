eea65cfc473efc21e9ca95cdeaf219a07210efb8
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-11-20 12:42:36 +0300

    Reflect changes in the Nodos API

--- Source/NOSSceneTreeManager/Public/NOSActorProperties.h
+++ Source/NOSSceneTreeManager/Public/NOSActorProperties.h
@@ -255,7 +255,7 @@ public:
 
 	virtual void SetArrayPropValues(void* val, size_t size, FScriptArrayHelper_InContainer& ArrayHelper) override
 	{
-		auto vec = (flatbuffers::Vector<u8>*)val; 
+		auto vec = (flatbuffers::Vector<uint8_t>*)val; 
 		int ct = vec->size();
 		ArrayHelper.Resize(ct);
 		for(int i = 0; i < ct; i++)
@@ -448,7 +448,7 @@ public:
 
 	virtual void SetArrayPropValues(void* val, size_t size, FScriptArrayHelper_InContainer& ArrayHelper) override
 	{
-		auto vec = (flatbuffers::Vector<u8>*)val; 
+		auto vec = (flatbuffers::Vector<uint8_t>*)val;
 		int ct = vec->size();
 		ArrayHelper.Resize(ct);
 		for(int i = 0; i < ct; i++)
