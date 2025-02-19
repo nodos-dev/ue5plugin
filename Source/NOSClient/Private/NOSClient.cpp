9486846e903d2184c9b652124d4de708bec6c8f0
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2025-02-19 12:31:40 +0300

    Use latest Nodos Process SDK version

--- Source/NOSClient/Private/NOSClient.cpp
+++ Source/NOSClient/Private/NOSClient.cpp
@@ -64,7 +64,7 @@ FString FNodos::GetNodosSDKDir()
 	{
 		return "";
 	}
-	FPlatformProcess::ExecProcess(*NosmanPath, TEXT("sdk-info 17.1.0 process"), &ReturnCode, &OutResults, &OutErrors, *NosmanWorkingDirectory);
+	FPlatformProcess::ExecProcess(*NosmanPath, TEXT("sdk-info 18.0.0 process"), &ReturnCode, &OutResults, &OutErrors, *NosmanWorkingDirectory);
 	LOGF("Nodos SDK path is %s", *OutResults);
 
 	TSharedPtr<FJsonObject> SDKInfoJsonParsed;
