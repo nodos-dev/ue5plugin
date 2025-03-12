622e0cf76f98b365faae031af41739e8ec1e7ab4
Caner <caner.coskun@nodos.dev>
2025-03-12 11:47:54 +0300

    Remove deprecated RefreshPortals request

--- Source/NOSSceneTreeManager/Private/NOSSceneTreeManager.cpp
+++ Source/NOSSceneTreeManager/Private/NOSSceneTreeManager.cpp
@@ -1713,12 +1713,6 @@ void FNOSSceneTreeManager::OnNOSNodeImported(nos::fb::Node const& appNode)
 		}
 	}
 	//SendSyncSemaphores(true);
-	flatbuffers::FlatBufferBuilder fb5;
-	auto offset4 = nos::CreateAppEventOffset(fb5, nos::CreateRefreshPortals(fb5));
-	fb5.Finish(offset4);
-	auto buf5 = fb5.Release();
-	auto root5 = flatbuffers::GetRoot<nos::app::AppEvent>(buf5.data());
-	NOSClient->AppServiceClient->Send(*root5);
 	LOG("Node from Nodos successfully imported");
 }
 
