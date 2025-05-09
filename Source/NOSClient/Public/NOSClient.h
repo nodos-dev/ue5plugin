0583c606c286b37cb2b887ddbaff52c489a43c17
Caner <caner.coskun@nodos.dev>
2025-05-09 14:24:49 +0300

    From 5.5nos1.2: Fix not waiting enough for new frame number with when delta seconds is high
    (cherry picked from commit a59e5d3381c1976823650c4151c4735a392ef049)

--- Source/NOSClient/Public/NOSClient.h
+++ Source/NOSClient/Public/NOSClient.h
@@ -30,10 +30,10 @@ struct ExecuteInfo
 };
 struct ExecuteFrameNumberQueue : public TQueue<ExecuteInfo>
 {
-	ExecuteInfo PopFrameNumber(uint64_t frameNumber)
+	ExecuteInfo PopFrameNumber(uint64_t frameNumber, float maxWaitTime)
 	{
 		ExecuteInfo executeInfo{};
-		DiscardExcessThenDequeue(executeInfo, frameNumber, true);
+		DiscardExcessThenDequeue(executeInfo, frameNumber, true, maxWaitTime);
 		return executeInfo;
 	}
 	void EnqueueExecuteStart(nos::app::AppExecuteStart const* appExecuteStart)
@@ -55,12 +55,13 @@ struct ExecuteFrameNumberQueue : public TQueue<ExecuteInfo>
 			Enqueue(std::move(start));
 	}
 private:
-	void DiscardExcessThenDequeue(ExecuteInfo& result, uint64_t requestedFrameNumber, bool wait)
+	void DiscardExcessThenDequeue(ExecuteInfo& result, uint64_t requestedFrameNumber, bool wait, float maxWaitTime)
 	{
 		std::scoped_lock lock(Guard);
 		uint32_t tryCount = 0;
 		bool dequeued = false;
 		bool oldLiveNow = LiveNow;
+		constexpr int retryCount = 20;
 		FPlatformProcess::ConditionalSleep([&]()
 			{
 				while (Peek(result))
@@ -78,8 +79,8 @@ private:
 					}
 				}
 
-				return !LiveNow || !wait || tryCount++ > 20;
-			}, 0.001f);
+				return !LiveNow || !wait || tryCount++ > retryCount;
+			}, maxWaitTime / retryCount);
 
 		LiveNow = dequeued;
 		if (oldLiveNow != LiveNow)
