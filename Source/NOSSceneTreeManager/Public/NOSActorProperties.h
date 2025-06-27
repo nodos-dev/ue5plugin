42da6c402c38182400ae0a368c5a5514c066e8aa
Caner <caner.coskun@nodos.dev>
2025-06-27 12:14:11 +0300

    Refactor

--- Source/NOSSceneTreeManager/Public/NOSActorProperties.h
+++ Source/NOSSceneTreeManager/Public/NOSActorProperties.h
@@ -220,20 +220,17 @@ public:
 		TypeName = LitType.val;
 		if constexpr (!std::is_same_v<CppType, bool>)
 		{
-			if (UIMinString.Len())
-			{
-				CppType UIMin = TNumericLimits<CppType>::Lowest();
-				TTypeFromString<CppType>::FromString(UIMin, *UIMinString);
-				min_val = std::vector<uint8_t>(sizeof(CppType), 0);
-				memcpy(min_val.data(), &UIMin, sizeof(CppType));
-			}
-			if (UIMaxString.Len())
-			{
-				CppType UIMax = TNumericLimits<CppType>::Max();
-				TTypeFromString<CppType>::FromString(UIMax, *UIMaxString);
-				max_val = std::vector<uint8_t>(sizeof(CppType), 0);
-				memcpy(max_val.data(), &UIMax, sizeof(CppType));
-			}
+			auto constexpr parseStrValueToVec = [](const FString& str, std::vector<uint8_t>& vec, CppType initialValue) {
+				if (str.Len())
+				{
+					CppType val = initialValue;
+					TTypeFromString<CppType>::FromString(val, *str);
+					vec = std::vector<uint8_t>(sizeof(CppType), 0);
+					memcpy(vec.data(), &val, sizeof(CppType));
+				}
+			};
+			parseStrValueToVec(UIMinString, min_val, TNumericLimits<CppType>::Lowest());
+			parseStrValueToVec(UIMaxString, max_val, TNumericLimits<CppType>::Max());
 		}
 	}	   
 	T* Property;
