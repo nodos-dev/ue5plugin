8f21b3f597a2e8554d46b5fdeb88016348613712
Furkan Can TURAN <fcturan10@gmail.com>
2025-07-29 15:22:51 +0300

    Depend on bumped major of the SDK

--- Source/NOSClient/Private/NOSClient.cpp
+++ Source/NOSClient/Private/NOSClient.cpp
@@ -63,7 +63,7 @@ FString FNodos::GetNodosSDKDir()
 	{
 		return "";
 	}
-	FPlatformProcess::ExecProcess(*NosmanPath, TEXT("sdk-info 18.0.0 process"), &ReturnCode, &OutResults, &OutErrors, *NosmanWorkingDirectory);
+	FPlatformProcess::ExecProcess(*NosmanPath, TEXT("sdk-info 19.0.0 process"), &ReturnCode, &OutResults, &OutErrors, *NosmanWorkingDirectory);
 	LOGF("Nodos SDK path is %s", *OutResults);
 
 	TSharedPtr<FJsonObject> SDKInfoJsonParsed;
